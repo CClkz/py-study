3d86175be9496570bd15bdc202e0438cbd8f8961
cclkz <7847034+cclkz@user.noreply.gitee.com>
2025-04-30 17:55:24 +0800

    feat: 调整目录结构

---
+++ src/algorithm/two-sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if nums[i]+nums[j] == target:
+                    return [i, j]
+
+    def twoSum1(self, nums, target):
+        dics = {}
+        for i, v in enumerate(nums):
+            if (target - v) in dics:
+                return [dics[target - v], i]
+            else:
+                dics[v] = i
